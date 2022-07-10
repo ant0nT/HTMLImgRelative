d954fe30d91196c7165413291008890a77151fcc
ant0nT <30862396+ant0nT@users.noreply.github.com>
2022-07-10 12:54:13 +0300

    The little changes in code Adding docstrings and some comments The little changes in README.md

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# HTMLImgRelative
-### The script scans html file in specified and child directories, then replaces remote images paths with local ones and downloads them to the local drive. ###
-### May be useful for building archives with offline html documentation. ###
+### HTMLImgRelative
+The script scans html file in specified and child directories, then replaces remote images paths with local ones and downloads them to the local drive.
+May be useful for building archives with offline html documentation.
 
-# Usage:
+### Usage:
 
-`python3 HTMLImgRelative /path/to/directory/with/html/files`
+`python3 HTMLImgRelative.py /path/to/directory/with/html/files`
 
 Functionality has been tested on python 3.8 and macos. Other systems will test soon.
  
-# Requirements:
+### Requirements:
  
 * bs4
 * requests
--- main.py
+++ main.py
@@ -10,10 +10,20 @@ directory_path = ''
 
 
 def download_image(i, src, rel_path):
-    #result = ''
+    """
+    Function downloads images and saves they in img/HTMLImgRelative + rel_path
+
+    :param i: ordinal number for images
+    :param src: images url
+    :param rel_path: relative path from directory path to html file)
+    :return: local path where image has been saved
+    """
+    # calculating directory path for downloading images
     img_dir_path = os.path.join(directory_path, 'img/HTMLImgRelative/', rel_path,)
+    # creating directories if they don't exist
     os.makedirs(img_dir_path, exist_ok=True)
     file_extension = pathlib.Path(src).suffix
+    # calculating file path. File name is ordinal number + file extension
     path = os.path.join(img_dir_path, str(i) + file_extension)
     img_data = requests.get(src).content
     with open(path, 'wb') as handler:
@@ -23,12 +33,17 @@ def download_image(i, src, rel_path):
 
 
 def find_imgs(html):
-    if os.path.basename(html).startswith('._'):
-        return
+    """
+    Function finds images tags, call download function and replaces source paths to locals
+
+    :param html: path to html file
+    :return: None
+    """
     try:
         soup = BeautifulSoup(open(html))
     except:
         print ('error file ' + str(html))
+        return
     images = soup.findAll('img')
     rel_path = os.path.relpath(html, directory_path)
     print(html)
@@ -38,16 +53,17 @@ def find_imgs(html):
                 print('src', img['src'])
                 path_to_image = download_image(i, img['src'], rel_path)
                 print(path_to_image)
-                #new_relative = os.path.relpath(path_to_image, )
                 img['src'] = str(os.path.relpath(path_to_image, os.path.dirname(html)))
         pass
     with open(html, "w", encoding='utf-8') as file:
         file.write(str(soup))
-    #return images
     pass
 
 
 def find_htmls():
+    """
+    :return: list of paths contained "**/*.html"
+    """
     file_dir = directory_path
     file_ext = '**/*.html'
     return list(pathlib.Path(file_dir).glob(file_ext))
@@ -57,12 +73,14 @@ def find_htmls():
 def main():
     htmls_list = find_htmls()
     for html in htmls_list:
-        if os.path.isfile(html):
+        # exclude not files and finder (macos) service files
+        if os.path.isfile(html) and not os.path.basename(html).startswith('._'):
             find_imgs(html)
     pass
 
 
 if __name__ == '__main__':
+    # path to parent dir. Global var
     directory_path = sys.argv[1]
     main()
     pass
